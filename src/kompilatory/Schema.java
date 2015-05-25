c4c3c4e41d786b83f28a0eea25403bb02b38f4b3
Agnieszka Szczurek <agnieszkaszczurek93@gmail.com>
2015-05-26 00:21:39 +0200

    Made parser schema to String

--- src/kompilatory/Schema.java
+++ src/kompilatory/Schema.java
@@ -1,14 +1,15 @@
 package kompilatory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Schema {
-	private Map<String, List<Map<String, String> > > schema;
-	
-	public Schema(){
-		schema = new HashMap<String, List<Map<String,String>>>();
+	private Map<String, List<Map<String, String>>> schema;
+
+	public Schema() {
+		schema = new HashMap<String, List<Map<String, String>>>();
 	}
 
 	public Map<String, List<Map<String, String>>> getSchema() {
@@ -22,7 +23,34 @@ public class Schema {
 	@Override
 	public String toString() {
 //		TODO:
-		return "Schema [schema=" + schema + "]";
+		StringBuilder builder = new StringBuilder();
+		builder.append("[\r\n");
+		for (Map.Entry map : schema.entrySet()) {
+			String mapKey = map.getKey().toString();
+
+			builder.append("\t\"").append(map.getKey().toString()).append("\": ");
+			builder.append("[");
+
+			List<Map<String, String>> lista = schema.get(mapKey);
+
+			for (Map<String, String> aLista : lista) {
+				builder.append("\r\n");
+				builder.append("\t\t{");
+				builder.append("\r\n");
+				for (Map.Entry values : aLista.entrySet()) {
+					builder.append("\t\t\t");
+					builder.append("\"").append(values.getKey().toString()).append("\": ").append("\"").append(values.getValue().toString()).append("\"");
+					builder.append("\r\n");
+				}
+
+				builder.append("\t\t}");
+			}
+
+
+			builder.append("\r\n\t],\r\n");
+		}
+		builder.append("]");
+		return builder.toString();
 	}
 
 	public void addTable(StringBuilder key, List<Map<String, String>> array) {
